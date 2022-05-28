6d1a822626497e37091c260b359f097452503b73
[TAREQUE-AHMED] <[tarequeahmed46@gmail.com]>
2022-05-28 23:05:45 +0600

    change in banner

--- src/Components/Pages/Home/Banner/Banner.js
+++ src/Components/Pages/Home/Banner/Banner.js
@@ -7,9 +7,9 @@ const Banner = () => {
         <div className="hero-content flex-col lg:flex-row-reverse">
             <img src="https://api.lorem.space/image/movie?w=260&h=400" className="max-w-sm rounded-lg shadow-2xl" />
             <div>
-            <h1 className="text-5xl font-bold">Box Office News!</h1>
-            <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-            <button className="btn btn-primary">Get Started</button>
+            <h1 className="text-5xl font-bold">Laptop Pro!</h1>
+            <p className="py-6">Laptop pro is multi nation laptop parts seller company....</p>
+            <button className="btn btn-primary">Read More</button>
             </div>
         </div>
         </div>
